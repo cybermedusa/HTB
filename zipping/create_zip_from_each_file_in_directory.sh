01a64e3b109c4368d0e247fd787ede9be06f12a5
cyber medusa <zuzannakilar@gmail.com>
2024-01-12 16:03:26 +0100

    corrected script

--- zipping/create_zip_from_each_file_in_directory.sh
+++ zipping/create_zip_from_each_file_in_directory.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 
-# Directory containing the files
 directory="/path/to/your/directory"
 
-# Change to the directory
 cd "$directory"
 
-# Loop through each file in the directory
 for file in *; do
-    # Check if it's a file and not a directory
     if [ -f "$file" ]; then
-        # Create a zip file for each file
-        zip "${file%.*}.zip" "$file"
+        zip "${file}.zip" "$file"
     fi
 done
